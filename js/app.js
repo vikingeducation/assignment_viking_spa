76bf3732b20979ca02a722cc3fe68fac77429d28
alokp <alokpra@gmail.com>
2015-09-16 11:29:10 -0700

    version control

--- js/app.js
+++ js/app.js
@@ -1,1 +1,7 @@
-var clothingStore = angular.module('clothingStore',['ui.router']);
+var clothingStore = angular.module('clothingStore', ['ui.router']);
+
+
+// Error handling
+clothingStore.run(function($rootScope){
+  $rootScope.$on('$stateChangeError', console.log.bind(console));
+});
