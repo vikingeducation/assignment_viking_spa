f5800f5b63c2df5ba18c4898bcc68f06091a88bc
shadefinale <shadefinale@gmail.com>
2015-09-16 19:02:15 -0500

    Add reviews and some styling.

--- js/services/productService.js
+++ js/services/productService.js
@@ -4,6 +4,18 @@ app.factory("productService", [ function(){
   _products = [];
   //_product={name: ,price: ,description: , category: }
 
+  var generateReviews = function(){
+    var reviews = [0,0,0,0];
+    return reviews.map(function(){ return generateReview() });
+  }
+
+  var generateReview = function(){
+    var review = {};
+    review.author = faker.name.findName();
+    review.text = faker.lorem.paragraph();
+
+    return review
+  }
   for (var i = 0; i < 15; i++){
     var newCategory = faker.commerce.department();
     if (_categories.indexOf(newCategory) == -1){
@@ -13,13 +25,14 @@ app.factory("productService", [ function(){
 
   for (var i = 0; i < 36; i++){
     var newProduct={id: i,
-              name: faker.commerce.productName(),
-              price: faker.commerce.price(),
-              description: faker.lorem.paragraph(),
-              category: _categories[Math.floor(Math.random()*_categories.length)],
-              image: faker.image.technics()};
+                    name: faker.commerce.productName(),
+                    price: faker.commerce.price(),
+                    description: faker.lorem.paragraph(),
+                    category: _categories[Math.floor(Math.random()*_categories.length)],
+                    image: faker.image.technics(),
+                    reviews: generateReviews(),
+                  };
     _products.push(newProduct);
-
   }
 
   obj.getCategories = function(){
