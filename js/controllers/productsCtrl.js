c6b9dcf5f2ec19a5298db376e329cbd330132359
Adam <adam.kinson@gmail.com>
2015-10-05 22:00:18 -0400

    add filtering dropdown

--- js/controllers/productsCtrl.js
+++ js/controllers/productsCtrl.js
@@ -1,6 +1,12 @@
 vikingStore.controller('ProductsCtrl',
   ['$scope', 'productService', function($scope, productService) {
 
-    $scope.products = productService.getProducts();
+    $scope.products = productService.products;
+    $scope.categories = productService.categories;
+
+
+    $scope.filterOn = function(category_id) {
+      $scope.categoryFilter = { category: { id: category_id } };
+    };
 
 }]);
