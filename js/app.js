a34dadefdcf647a211546fa103911ca6c3188de7
Sam O'Keefe <samok13@gmail.com>
2016-03-28 10:05:16 -0700

    products template

--- js/app.js
+++ js/app.js
@@ -1,1 +1,23 @@
-var store = angular.module('widgets', []);
+var store = angular.module('store', ['ui.router']);
+
+store.config(function($stateProvider, $urlRouterProvider){
+
+   $urlRouterProvider.otherwise("");
+
+   $stateProvider
+
+    .state('landing', {
+      url: '',
+      templateUrl: 'js/templates/landing.html',
+    })
+
+    .state('landing.products',{
+      url: '/products',
+      templateUrl: 'js/templates/products/index.html'
+    })
+
+
+})
+
+
+ 
