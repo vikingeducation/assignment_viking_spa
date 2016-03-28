61174153476381f5b6c85b1263505a674413e699
Andrew Baik <and.baik@gmail.com>
2016-03-28 18:04:12 -0400

    working on category filter

--- js/controllers/productController.js
+++ js/controllers/productController.js
@@ -1,9 +1,7 @@
 store.controller('productCtrl',
 ['$scope', 'categoryService', 'productService',
   function($scope, categoryService, productService) {
-
     $scope.categoryList = categoryService.generateCategoryList();
     $scope.productList = productService.generateProductList();
-
   }
 ]);
