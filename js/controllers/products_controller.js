77329d453adbbe7355cb899517ad41a839cbc955
cjvirtucio87 <cjv287@gmail.com>
2016-09-28 11:09:25 -0700

    Seeded products and categories.

--- js/controllers/products_controller.js
+++ js/controllers/products_controller.js
@@ -1,17 +1,28 @@
 app.controller('ProductsCtrl',
 ['$scope', 'ProductsService', 'CategoriesService', function($scope, ProductsService, CategoriesService) {
 
-  CategoriesService.seed().then(function(data){
-    $scope.categories = data;
-    ProductsService.seed().then(function(data) {
+  CategoriesService
+    .seed()
+    .then(function(data){
+      $scope.categories = data;
+    })
+    .then(ProductsService.seed)
+    .then(function(data) {
       $scope.products = data;
+    })
+    .then(function(data) {
+      console.log($scope.products);
     });
-  });
-
 
 
   // ProductsService.getAll().then(function(data){
   //   $scope.products = data;
   // });
 
+
+  // ProductsService.seed().then(function(data) {
+  //   $scope.products = data;
+  // }).then(function(data) {
+  //   console.log($scope.products);
+  // });
 }]);
--- js/services/products_service.js
+++ js/services/products_service.js
@@ -4,26 +4,25 @@ app.factory('ProductsService', ['faker', 'CategoriesService', function(faker, Ca
   var _product_id = 0;
   var ProductsService = {};
 
-  function _seedProduct() {
+  function _seedProduct(prodId) {
+    var productId = prodId;
     var newProduct =
-      {
-        id: _product_id,
-        name: faker.commerce.product(),
-        description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-        price: faker.commerce.price(),
-        stock: faker.random.number()
-      };
-    CategoriesService.getAll().then(function(data){
-        newProduct.category_id =  Math.floor(Math.random()* Object.keys(data).length);
-        _products[_product_id.toString()] = newProduct;
-        CategoriesService.updateCategoryProducts(newProduct.category_id, newProduct.id);
-    });
-    _product_id++;
+    {
+      id: productId,
+      name: faker.commerce.product(),
+      description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+      price: faker.commerce.price(),
+      stock: faker.random.number()
+    };
+    newProduct.category_id = Math.floor(Math.random()*5);
+    _products[_product_id.toString()] = newProduct;
+    CategoriesService.updateCategoryProducts(newProduct.category_id, newProduct.id);
   }
 
   function _seedProducts(count) {
     for (var i = 0; i < count; i++) {
-      _seedProduct();
+      _seedProduct(_product_id);
+      _product_id = _product_id + 1;
     }
     return Promise.resolve(_products);
   }
