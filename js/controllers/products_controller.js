fdbeee5d4797f079bad5e7e436a372b9fd566eb5
jsteenb2 <jonathansteenbergen@gmail.com>
2016-09-28 13:58:36 -0700

    added sort

--- js/controllers/products_controller.js
+++ js/controllers/products_controller.js
@@ -2,7 +2,25 @@ app.controller('ProductsCtrl',
 ['$scope',
 'products',
 'categories',
-function($scope, products, categories) {
+'_',
+function($scope, products, categories, _) {
+  $scope.sortegory = "";
   $scope.products = products;
   $scope.categories = categories;
+
+  $scope.getProducts = function(){
+    if ($scope.sortegory){
+      console.log($scope.sortegory)
+      var product_ids = $scope.categories[$scope.sortegory].product_ids;
+
+      var filtered = [];
+      product_ids.forEach(function(el){
+        filtered.push($scope.products[el]);
+      });
+
+      return filtered;
+    } else {
+      return $scope.products;
+    }
+  }
 }]);
