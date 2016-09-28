6cdfee0414bf1a7e779b73fdc7151a6a09c0637a
cjvirtucio87 <cjv287@gmail.com>
2016-09-28 14:54:17 -0700

    working on carts

--- js/services/carts_service.js
+++ js/services/carts_service.js
@@ -1,39 +1,21 @@
-app.factory('CategoriesService', ['faker', function(faker) {
-  var _category_id = 0;
-  var _categories = {};
+app.factory('CartsService', ['faker', function(faker) {
 
-  var CategoriesService = {};
+  var _cart = {};
 
-  function _seedCategory(){
-    var newCategory = {
-      id: _category_id,
-      name: faker.random.word(),
-      product_ids: []
-    };
-    _categories[_category_id.toString()] = newCategory;
-    _category_id++;
-  }
+  var addItem = function(item) {
+    _cart[item.id] = item;
+  };
 
-  function _seedCategories(count){
-    for (var i = 0; i < count; i++){
-      _seedCategory();
+  var removeItem = function(item_id) {
+    delete _cart[item_id];
+  };
+
+  return {
+    addItem: addItem,
+    removeItem: removeItem,
+    getCart: function() {
+      return _cart;
     }
-    return Promise.resolve(_categories);
-  }
-
-  CategoriesService.seed = function() {
-    return _seedCategories(5);
   };
 
-  CategoriesService.getAll = function(){
-    return Promise.resolve(_categories);
-  };
-
-  CategoriesService.updateCategoryProducts = function(catId, prodId){
-    console.log(catId);
-    console.log(prodId);
-    _categories[catId].product_ids.push(prodId);
-  };
-
-  return CategoriesService;
 }]);
