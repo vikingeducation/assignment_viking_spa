06dd6c236f575dcdff671583a63619bc44d20db9
cjvirtucio87 <cjv287@gmail.com>
2016-09-28 12:39:29 -0700

    refactored to use resolve object

--- js/app.js
+++ js/app.js
@@ -11,16 +11,29 @@ app.factory('faker', ['$window', function($window) {
 app.config(function($stateProvider, $urlRouterProvider){
 
   $urlRouterProvider.otherwise("/products");
-  
+
   $stateProvider
     .state('products', {
       url: '/products',
       abstract: true,
       template: '<div ui-view></div>',
+      controller: ''
     })
     .state('products.index', {
       url: '',
       templateUrl: 'js/templates/products/index.html',
-      controller: 'ProductsCtrl'
+      controller: 'ProductsCtrl',
+      resolve: {
+        categories: function (CategoriesService) {
+          return CategoriesService.seed().then(function(data) {
+            return data;
+          });
+        },
+        products: function (ProductsService) {
+          return ProductsService.seed().then(function(data){
+            return data;
+          });
+        }
+      }
     });
 });
--- js/controllers/products_controller.js
+++ js/controllers/products_controller.js
@@ -1,16 +1,8 @@
 app.controller('ProductsCtrl',
-['$scope', 'ProductsService', 'CategoriesService', function($scope, ProductsService, CategoriesService) {
-
-  CategoriesService
-    .seed()
-    .then(function(data){
-      $scope.categories = data;
-    })
-    .then(ProductsService.seed)
-    .then(function(data) {
-      $scope.products = data;
-    })
-    .then(function(data) {
-      console.log($scope.products);
-    });
+['$scope',
+'products',
+'categories',
+function($scope, products, categories) {
+  $scope.products = products;
+  $scope.categories = categories;
 }]);
