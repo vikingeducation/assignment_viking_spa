6b21c22b8caaaa3d5584353a2f26439c377e9c52
Steven Chang <prime_pork@hotmail.com>
2016-09-29 17:07:53 +1000

    made the footer into a directive / template

---
+++ js/directives/main_footer.js
@@ -0,0 +1,12 @@
+//---------------
+// mainFooter
+//---------------
+
+VikingStore.directive('mainFooter', function(){
+
+	return {
+		templateUrl: "/js/templates/shared/main_footer.html",
+		scope: true
+	};
+
+});
