14f7813542f680c07dfdfcab61ce36d3a68b92f4
Steven Chang <prime_pork@hotmail.com>
2016-10-01 01:34:02 +1000

    product filtering is now functional

--- js/controllers/products_controller.js
+++ js/controllers/products_controller.js
@@ -24,11 +24,33 @@ VikingStore.controller("ProductsController", ["$scope", 'CategoriesService', 'Pr
 
 	var _productsInObject = ProductsService.all();
 
+	var _returnProductsFromCategory = function(){
+		var array = [];
+		var products_ids = $scope.categories[$scope.category.id].products_ids;
+		for (var i = 0; i < products_ids.length; i++){
+			array.push(_productsInObject[products_ids[i] + ""]);
+		};
+		return array;
+	};
+
 	// ------------
 	// Public
 	// ------------
 
+	$scope.category = {id: "all"};
+
 	$scope.categories = CategoriesService.all();
+
+	$scope.filterByCategory = function(){
+		var array;
+		if ($scope.category.id === "all"){
+			array = _moveProductsToArray(_productsInObject);
+		} else {
+			array = _returnProductsFromCategory();
+		};
+
+		$scope.productsInChunks = _chunk(array, 3)
+	};
 
 	// Converting products into  groups.
 	$scope.productsInChunks = _chunk(_moveProductsToArray(_productsInObject), 3)
