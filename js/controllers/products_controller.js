c35a9773c81533147d4c5db918b4b7503fca1607
Steven Chang <prime_pork@hotmail.com>
2016-10-01 00:12:57 +1000

    fixed up issues in products_controller that was causing infdig errors

--- js/controllers/products_controller.js
+++ js/controllers/products_controller.js
@@ -1,28 +1,36 @@
 VikingStore.controller("ProductsController", ["$scope", 'CategoriesService', 'ProductsService', function($scope, CategoriesService, ProductsService){
 
-	var _productsInObject = ProductsService.all();
+	// ------------
+	// Private
+	// ------------
 
-	// ProductsController.moveProductsToArray
-	var _moveProductsToArray = function(){
-		$scope.productsInArray = [];
-		var keys = Object.keys(_productsInObject);
-		for(var i = 0;i < keys.length; i++){
-			$scope.productsInArray.push(_productsInObject[keys[i]]);
-		};
-	};
-
-	_moveProductsToArray();
-
-	$scope.categories = CategoriesService.all();
-
-	$scope.chunk = function(array, size){
+	var _chunk = function(array, size){
 		var newArray = [];
 		for (var i=0; i < array.length; i += size){
 			newArray.push(array.slice(i, i+ size));
 		};
-		return newArray
+		return newArray;
 	};
 
-	$scope.productsInArray;
+	// ProductsController.moveProductsToArray
+	var _moveProductsToArray = function(object){
+		var array = [];
+		var keys = Object.keys(object);
+		for(var i = 0;i < keys.length; i++){
+			array.push(object[keys[i]]);
+		};
+		return array;
+	};
+
+	var _productsInObject = ProductsService.all();
+
+	// ------------
+	// Public
+	// ------------
+
+	$scope.categories = CategoriesService.all();
+
+	// Converting products into  groups.
+	$scope.productsInChunks = _chunk(_moveProductsToArray(_productsInObject), 3)
 
 }])
