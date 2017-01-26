713404e02d3d26249f0b07b967189f1e0f97395f
Jessica G <gillan.jessica@gmail.com>
2017-01-26 15:35:16 -0700

    finish address form

--- js/controllers/CartCtrl.js
+++ js/controllers/CartCtrl.js
@@ -2,4 +2,5 @@ store.controller('CartCtrl', ['$scope', '$stateParams', 'productService', 'cartS
   function($scope, $stateParams, productService, cartService) {
     $scope.cart = cartService.get()
     $scope.totalItems = cartService.count;
+  
   }]);
--- js/controllers/CheckoutCtrl.js
+++ js/controllers/CheckoutCtrl.js
@@ -1,7 +1,10 @@
-store.controller('CheckoutCtrl', ['$scope', 'cartService',
-  function($scope, cartService) {
-    var checkValid = function(form){
-      console.log("ran")
-      console.log(form)
+store.controller('CheckoutCtrl', ['$scope', '$state', 'cartService',
+  function($scope, $state, cartService) {
+
+    $scope.checkValid = function(form){
+      console.log("form in checkValid", form)
+      if(form.$valid) {
+        $state.go('checkout.payment')
+      }
     }
   }]);
