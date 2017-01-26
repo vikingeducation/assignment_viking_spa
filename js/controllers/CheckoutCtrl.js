a87805a110ff3f6d304a09c11c81f0057813a809
Francisco Suero <suerofrancisco@yahoo.com>
2017-01-26 18:34:00 -0500

    forms!

--- js/controllers/CheckoutCtrl.js
+++ js/controllers/CheckoutCtrl.js
@@ -2,10 +2,16 @@ store.controller('CheckoutCtrl', ['$scope', '$state', 'cartService',
   function($scope, $state, cartService) {
 
     $scope.formData = {};
+    $scope.sameAsShipping = false
 
-    $scope.checkValid = function(form, state){
+    $scope.checkValid = function(form, state, sameAsShipping){
       console.log("form in checkValid", form)
       if(form.$valid) {
+        if(form.$name === "paymentForm" && sameAsShipping){
+          console.log("We're here!")
+          $scope.formData.billingAddress = $scope.formData.billingAddress || {}
+          angular.copy($scope.formData.address, $scope.formData.billingAddress)
+        }
         $state.go(state)
       }
     }
