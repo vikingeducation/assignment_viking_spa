3c0b8f22429bc9cf87821b00614eea6f8ccc649c
Kenneth Kottenstette <kennykotten@gmail.com>
2017-05-17 21:16:07 -0700

    basics working

--- javascripts/app.js
+++ javascripts/app.js
@@ -1,1 +1,31 @@
-var app = angular.module('VikingApp', []);
+var app = angular.module('VikingApp', ['ui.router']);
+
+app.config(function($stateProvider, $urlRouterProvider) {
+
+  // set a default state for all nonexistent routes
+  // this is your "else" state
+  $urlRouterProvider.otherwise("/route1");
+
+  // set up some states on $stateProvider
+  $stateProvider
+
+    // name the state
+    .state('state1', {
+
+      // set the path to your state
+      url: "/route1",
+
+      // set the template for your state
+      templateUrl: "javascripts/templates/route1.html",
+
+      // name an existing controller for your route
+      controller: "MainCtrl"
+    })
+
+    .state('state2', {
+      url: "/route2",
+      templateUrl: "javascripts/templates/route2.html",
+      controller: "MainCtrl"
+    })
+
+});
